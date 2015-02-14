e4600427c04de201b0eb6527b5587db364b5d74a
William Guo <willguo@berkeley.edu>
2015-02-14 07:10:57 -0400

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = '7f65573dfd8a557f4c7eaf4c1182b66b1d2d04aba052865eea741cd2aa109567b44a268bbd2ef4eb0b1ed712b441adad1db637611bbcc293b7c9bb7402c1bc1c'
